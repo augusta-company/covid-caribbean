b7059b5aa4147e2b5175ca9c107eb393bd5c0fa3
DavidNeiOs <davidneios25@gmail.com>
2020-03-20 20:55:03 -0400

    condition for window added

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,21 +19,22 @@ const IndexPage = () => (
     </p>
 
     <script type="text/javascript">
-      {(function(d, m) {
-        var kommunicateSettings = {
-          appId: "39fd4adf1615bc0a0ec2a4bc20738caac",
-          popupWidget: true,
-          automaticChatOpenOnNavigation: true,
-        }
-        var s = document.createElement("script")
-        s.type = "text/javascript"
-        s.async = true
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app"
-        var h = document.getElementsByTagName("head")[0]
-        h.appendChild(s)
-        window.kommunicate = m
-        m._globals = kommunicateSettings
-      })(document, window.kommunicate || {})}
+      {Boolean(window) &&
+        (function(d, m) {
+          var kommunicateSettings = {
+            appId: "39fd4adf1615bc0a0ec2a4bc20738caac",
+            popupWidget: true,
+            automaticChatOpenOnNavigation: true,
+          }
+          var s = document.createElement("script")
+          s.type = "text/javascript"
+          s.async = true
+          s.src = "https://widget.kommunicate.io/v2/kommunicate.app"
+          var h = document.getElementsByTagName("head")[0]
+          h.appendChild(s)
+          window.kommunicate = m
+          m._globals = kommunicateSettings
+        })(document, window.kommunicate || {})}
     </script>
   </Layout>
 )
