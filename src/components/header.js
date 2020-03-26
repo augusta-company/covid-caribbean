230e027c9d67ad7733fc49209e1e85300b077705
DavidNeiOs <davidneios25@gmail.com>
2020-03-26 11:32:11 -0400

    fix in header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rgb(41, 98, 169)`,
+      backgroundColor: `rgb(41, 98, 169)`,
       marginTop: `1.45rem`,
       marginBottom: `1.45rem`,
     }}
