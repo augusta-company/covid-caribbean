ac2f18f1c53b7416171d933a57c834f0e9d053ef
KerronKing <king_of_palmiste@hotmail.com>
2020-03-27 12:24:29 -0400

    Update mediumWarning component text and phone links

--- src/components/warnings/mediumWarning.js
+++ src/components/warnings/mediumWarning.js
@@ -5,25 +5,20 @@ export const MediumWarning = () => (
     <div className="card__info">
       <p>
         <span role="img" aria-label="doctor-health">
-          🧑🏿‍⚕️
+          ☎️
         </span>{" "}
-        <strong>Please consult your family doctor.</strong> If you are unable to
-        reach your regular health care provider, call{" "}
+        Please consult one of the below hotlines immediately:
+      </p>
+      <p>
+        COVID-19 National Hotline - <a href="tel:877-9355">877-9355</a> (877-WELL)<br/>
+        Southwest Region - <a href="tel:877-9724">877-9724</a> (87-SWRHA)<br/>
+        Tobago - <a href="tel:800-4325">800-4325</a> (800-HEAL)
+      </p>
+      <p>
         <strong>
-          <a href="tel:877-9355">877-9355</a> (877-WELL)
+          and a medical professional will advise you regarding isolation
+          and notice to employers.
         </strong>
-        .
-      </p>
-      <p>
-        Someone can speak with you about your symptoms and provide health
-        advice.
-      </p>
-
-      <p>
-        For Global News, The World Health Organization has created a phone
-        service to provide <strong>non-medical</strong> information about
-        COVID-19. Information is available via text message or Whatsapp or at 
-        <strong>+41 79 893 18 92.</strong>
       </p>
     </div>
   </div>
