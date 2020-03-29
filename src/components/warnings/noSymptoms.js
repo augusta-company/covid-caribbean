d3c7fb146833a1d163b9c13068dfdc5296a04fe8
DavidNeiOs <davidneios25@gmail.com>
2020-03-29 18:16:21 -0400

    noSyptoms cmp updated

--- src/components/warnings/noSymptoms.js
+++ src/components/warnings/noSymptoms.js
@@ -1,38 +1,42 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
-export const NoSymptoms = () => (
-  <div key="noSymptoms" className="card">
-    <div className="card__info">
-      <h3>
-        <span role="img" aria-label="green-check">
-          ✅
-        </span>{" "}
-        Please stay at home. You do not need testing for COVID-19.
-      </h3>
-      <p>
-        <strong>
-          Since you don't have symptoms, you do not need testing for COVID-19 at
-          this time.
-        </strong>
-         However, there’s a chance you could get sick since your exposure. You
-        should self-monitor for any symptoms (fever, cough, sneezing, sore
-        throat, or difficulty breathing). If you begin to develop these, you
-        should take this self-assessment again.
-      </p>
-      <p>
-        If you are a health care worker, follow the advice of your employer.
-      </p>
-      <p>
-        If you need more information, go to{" "}
-        <a
-          href="http://www.health.gov.tt/sitepages/default.aspx?id=292"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TNT COVID-19 Website
-        </a>
-        .
-      </p>
+import { getHtmlforCountry } from "../../utils"
+
+export const NoSymptoms = (
+  dispatch,
+  index,
+  items,
+  symptoms,
+  country = "trinindad"
+) => {
+  const data = useStaticQuery(GetData)
+
+  const info = getHtmlforCountry(data.allMdx.edges, country)
+
+  return (
+    <div key="noSymptoms" className="card">
+      <div className="card__info">
+        <MDXRenderer>{info.node.body}</MDXRenderer>
+      </div>
     </div>
-  </div>
-)
+  )
+}
+
+const GetData = graphql`
+  query {
+    allMdx(filter: { frontmatter: { name: { regex: "/noSymptoms/" } } }) {
+      edges {
+        node {
+          body
+          frontmatter {
+            type
+            name
+            country
+          }
+        }
+      }
+    }
+  }
+`
